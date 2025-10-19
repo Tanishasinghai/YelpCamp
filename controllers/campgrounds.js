75d7efc91b09360c7cdc55a3a09d6cda32e7d0dd
Tanisha Singhai <52822131+Tanishasinghai@users.noreply.github.com>
2025-10-19 15:50:23 +0200

    Refactor campground controller to use OSM geocoding
    Removed Mapbox geocoding and updated to use OpenStreetMap for geocoding locations. Refactored various functions for consistency and clarity.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,8 +1,8 @@
-const Campground=require('../models/campground');
-const maxGeocoding=require('@mapbox/mapbox-sdk/services/geocoding');
-//const mapBoxToken=process.env.MAPBOX_TOKEN;
-//const geocoder=maxGeocoding({accessToken:mapBoxToken});
-//const {cloudinary}=require('../cloudinary');
+const Campground = require('../models/campground');
+// ⛔️ REMOVED: Mapbox import
+// const maxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const { cloudinary } = require('../cloudinary'); // ✅ needed for deleteImages
+
 // --- OSM (Nominatim) geocoder: no API key needed ---
 async function geocodeOSM(query) {
   const url = new URL('https://nominatim.openstreetmap.org/search');
@@ -22,117 +22,98 @@ async function geocodeOSM(query) {
   return { type: 'Point', coordinates: [Number(data[0].lon), Number(data[0].lat)] };
 }
 
-
-module.exports.index=async (req,res,next)=>{
-    const campgrounds=await Campground.find({});
-    res.render('campgrounds/index',{campgrounds});
+module.exports.index = async (req, res, next) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
 };
 
-module.exports.renderNewForm=(req,res)=>{
-    res.render('campgrounds/new');
+module.exports.renderNewForm = (req, res) => {
+  res.render('campgrounds/new');
 };
 
-module.exports.createCampground=async (req,res,next)=>{
-   /* const geoData=await geocoder.forwardGeocode({
-        query:req.body.campground.location,
-        limit:1
-    }).send()*/
+module.exports.createCampground = async (req, res, next) => {
+  // ✅ Geocode with OSM (no keys)
+  let geometry = await geocodeOSM(req.body.campground.location);
+  if (!geometry) {
+    geometry = { type: 'Point', coordinates: [0, 0] };
+  }
+
+  const campground = new Campground(req.body.campground);
+  campground.geometry = geometry;
+  campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+  campground.author = req.user._id;
+
+  await campground.save();
+  req.flash('success', 'Successfully made a new campground!');
+  res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.showCampground = async (req, res, next) => {
+  const campground = await (await (await Campground.findById(req.params.id)
+    .populate({
+      path: 'reviews',
+      populate: { path: 'author' }
+    }))
+    .populate('author'));
+  if (!campground) {
+    req.flash('error', 'Cannot find that Campground!!');
+    return res.redirect('/campgrounds');
+  }
+  res.render('campgrounds/show', { campground });
+};
+
+module.exports.renderEditForm = async (req, res, next) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  if (!campground) {
+    req.flash('error', 'Cannot find that Campground!!');
+    return res.redirect('/campgrounds');
+  }
+  res.render('campgrounds/edit', { campground });
+};
+
+module.exports.updateCampground = async (req, res, next) => {
+  const { id } = req.params;
+
+  // Update campground main data
+  const campground = await Campground.findByIdAndUpdate(
+    id,
+    { ...req.body.campground },
+    { new: true }
+  );
+
+  // Add newly uploaded images
+  const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+  campground.images.push(...imgs);
+
+  // ✅ Re-geocode with OSM if location changed
+  if (req.body.campground.location) {
     let geometry = await geocodeOSM(req.body.campground.location);
     if (!geometry) {
-    // fallback if no location is found
-    geometry = { type: 'Point', coordinates: [0, 0] };
+      geometry = { type: 'Point', coordinates: [0, 0] };
     }
-    const campground=new Campground(req.body.campground);
-    //campground.geometry=geoData.body.features[0].geometry;
     campground.geometry = geometry;
-    campground.images=req.files.map(f=> ({ url:f.path, filename:f.filename}));
-    //if(!req.body.campground)throw new ExpressError('Invalid Campground data')    
-    campground.author=req.user._id;
-     await campground.save();
-     console.log(campground);
-     req.flash('success','Successfully made a new campground!');
-     res.redirect(`/campgrounds/${campground._id}`);  
- };
+  }
 
- module.exports.showCampground=async (req,res,next)=>{
-    const campground=await (await (await Campground.findById(req.params.id).populate({
-        path:'reviews',
-        populate:{
-            path:'author'
-        }
-    }).populate('author')));
-    if(!campground){
-        req.flash('error','Cannot find that Campground!!');
-        return res.redirect('/campgrounds');
+  await campground.save();
+
+  // Delete old images if selected
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
     }
-    res.render('campgrounds/show',{campground});
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } }
+    });
+  }
+
+  req.flash('success', 'Successfully updated campground!');
+  res.redirect(`/campgrounds/${campground._id}`);
 };
 
-module.exports.renderEditForm=async (req,res,next)=>{
-    const {id}=req.params;
-    const campground=await Campground.findById(id);
-    if(!campground){
-        req.flash('error','Cannot find that Campground!!');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/edit',{campground});
+module.exports.deleteCampground = async (req, res, next) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  req.flash('success', 'Successfully Deleted a Campground');
+  res.redirect('/campgrounds');
 };
-
-/*module.exports.updateCampground=async (req,res,next)=>{
-    const {id}=req.params; 
-    console.log(req.body);
-    const campground=await Campground.findByIdAndUpdate(id,{...req.body.campground})
-    const imgs=req.files.map(f=> ({ url:f.path, filename:f.filename}));
-    campground.images.push(...imgs);
-    await campground.save();
-    if(req.body.deleteImages){
-        for(let filename of req.body.deleteImages){
-            await cloudinary.uploader.destroy(filename);
-        }
-    await campground.updateOne({$pull:{images:{filename:{$in:req.body.deleteImages}}}})
-    console.log(campground);   
-}
-    req.flash('success','Succesfully update a Campground');
-   res.redirect(`/campgrounds/${campground._id}`);
-};*/
-module.exports.updateCampground = async (req, res, next) => {
-    const { id } = req.params;
-
-    // Update campground main data
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
-
-    // Add newly uploaded images
-    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
-    campground.images.push(...imgs);
-
-    // ✅ NEW: Update location using OpenStreetMap (no API key needed)
-    if (req.body.campground.location) {
-        let geometry = await geocodeOSM(req.body.campground.location);
-        if (!geometry) {
-            geometry = { type: 'Point', coordinates: [0, 0] }; // fallback to 0,0
-        }
-        campground.geometry = geometry;
-    }
-
-    await campground.save();
-
-    // Delete old images if selected
-    if (req.body.deleteImages) {
-        for (let filename of req.body.deleteImages) {
-            await cloudinary.uploader.destroy(filename);
-        }
-        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
-    }
-
-    req.flash('success', 'Successfully updated campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
-};
-
-
-module.exports.deleteCampground=async (req,res,next)=>{
-    const {id}=req.params;
-    await Campground.findByIdAndDelete(id);
-  req.flash('success','Successfully Deleted a Campground')
-    res.redirect('/campgrounds');
-};
-
